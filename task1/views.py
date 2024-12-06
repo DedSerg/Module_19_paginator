fcc5ec699403926843bccf6089c3fd0caba3024b
DedSerg <168771309+DedSerg@users.noreply.github.com>
2024-12-06 15:03:29 +0300

    Add files via upload

--- task1/views.py
+++ task1/views.py
@@ -1,1 +1,96 @@
+from django.shortcuts import render, redirect
 
+# Create your views here.
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from pyexpat.errors import messages
+from django.contrib import messages
+from .forms import UserRegister
+from .models import *
+
+
+class platform_game(TemplateView):
+    template_name = 'platform.html'
+
+
+class cart_game(TemplateView):
+    template_name = 'cart.html'
+
+
+def menu_game(request):
+    # mydict = {'games': ["Atomic Heart", "Cyberpunk 2077"]}
+    mydict = Game.objects.all().values()
+    context = {
+        'mydict': mydict,
+    }
+    return render(request, 'games.html', context)
+
+
+def sign_up_by_html(request):
+    users = []
+    users1 = Buyer.objects.all().values()
+    n_users = len(users1)
+    for i in range(n_users):
+        users.append(users1[i]['name'])
+
+    info = {}
+
+    if request.method == 'POST':
+        user_have = False
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = int(request.POST.get('age'))
+        is_user = username in users
+        if password == repeat_password:
+            if age >= 18:
+                if is_user == False:
+                    user_have = True
+                else:
+                    info['error'] = 'Такой покупатель уже существует'
+            else:
+                info['error'] = 'Вы должны быть старше 18 лет'
+        else:
+            info['error'] = 'Пароль не совпадает'
+
+        if user_have:
+            message = (f'Добро пожаловать, {username}!')
+            Buyer.objects.create(name=username, balance=0, age=age)
+        else:
+            message = info['error']
+        return HttpResponse(message)
+    return render(request, 'registration_page.html', info)
+
+
+def sign_up_by_django(request):
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = int(form.cleaned_data['age'])
+
+            # Проверка существования пользователя
+            if Buyer.objects.filter(name=username).exists():
+                messages.error(request, 'Такой покупатель уже существует.')
+                return render(request, 'registration_page.html', {'form': form})
+
+            if password != repeat_password:
+                messages.error(request, 'Пароли не совпадают.')
+                return render(request, 'registration_page.html', {'form': form})
+
+            if age < 18:
+                messages.error(request, 'Вы должны быть старше 18 лет.')
+                return render(request, 'registration_page.html', {'form': form})
+
+            # Если все проверки пройдены, зарегистрировать пользователя
+            Buyer.objects.create(name=username, balance=0, age=age)
+            messages.success(request, f'Добро пожаловать, {username}!')
+
+            return redirect('home')  # Перенаправление на главную страницу после успешной регистрации
+
+    else:
+        form = UserRegister()
+
+    return render(request, 'registration_page.html', {'form': form})
